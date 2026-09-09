5e1f4c8df279a5a96a0d546502aef33a281a47da
eva cheung <evacheung0929@gmail.com>
2026-09-09 20:27:18 +0100

    DATA: successfully returning the rows based on test sql

--- app/vulnerable.py
+++ app/vulnerable.py
@@ -6,3 +6,14 @@ This is a demonstartion of a vulnerable system
 
 '''
 
+from .db import run_sql
+# python -m app.db in terminal tells us what is in the table if forgotten
+
+usr_question = "SELECT * FROM customers"
+def run_vulnerable(question: str) -> dict:
+    sql_test = question
+    rows = run_sql(sql_test)
+    print(rows)
+    return rows
+
+run_vulnerable(usr_question)
