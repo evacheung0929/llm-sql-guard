1592a6aaff3bfadb5b3424ed7403e5c64a85376c
eva cheung <evacheung0929@gmail.com>
2026-09-09 20:13:48 +0100

    API: LLM call success

--- app/llm.py
+++ app/llm.py
@@ -6,8 +6,24 @@ This piece of code talks to calude api, returninig SQL
 
 import os
 from dotenv import load_dotenv
+import anthropic
 
 # reads .env and copies the values into os.environ
 load_dotenv()
-API_Key = os.environ(['ANTHROPIC_API_KEY'])
+API_Key = os.environ.get('ANTHROPIC_API_KEY')
+anthropic_client = anthropic.Client(api_key=API_Key)
 
+
+
+def test() -> str:
+    role = 'user'
+    message_content = "hi claude"
+    message = anthropic_client.messages.create(
+        model="claude-haiku-4-5",
+        messages=[{"role": role, "content": message_content}],
+        max_tokens=100,
+    )
+    print(message)
+
+    return message.content[0].text
+test()
